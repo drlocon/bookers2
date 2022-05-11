b3ef6c8d37f48eb5529f7f84bb887f0c976c82bd
drlocon <104292397+drlocon@users.noreply.github.com>
2022-05-11 07:56:36 +0000

    [Add]ActiveStorageを実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   
   config.hosts << "942d10ff0800433d98cd8c3fe69bedb1.vfs.cloud9.us-east-1.amazonaws.com"
+  
+  config.active_job.queue_adapter = :inline
 end
