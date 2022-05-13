9f36248851d4884804be70a1a2967939b9ff2faa
drlocon <104292397+drlocon@users.noreply.github.com>
2022-05-13 12:39:27 +0000

    [Add]indexページの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_one_attached :image
+  has_one_attached :profile_image
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+  
   has_many :books, dependent: :destroy
+  
+
+  
 end
