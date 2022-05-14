26acc1fd96f012f69dd66152a83afcca2ba7b3ef
drlocon <104292397+drlocon@users.noreply.github.com>
2022-05-14 07:34:53 +0000

    [Update]bookのindexページ更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,26 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def edit
+    @user = current_user
   end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
