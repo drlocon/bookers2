4cbfcfd3c5b35b50de31f66cefb48a1c896273fd
drlocon <104292397+drlocon@users.noreply.github.com>
2022-05-14 14:37:19 +0000

    [Update]各レイアウト調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def index
+    @user = User.find(current_user.id)
+    @users = User.all
   end
 
   def show
@@ -21,7 +23,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
