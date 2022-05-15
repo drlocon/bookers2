9662d923e12f42c3c07891cdc18e2a43299308ad
drlocon <104292397+drlocon@users.noreply.github.com>
2022-05-15 13:56:02 +0000

    [Add]アクセス制限の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,8 +30,8 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = 'You have updated book successfully.'
       redirect_to book_path
     else
@@ -40,9 +40,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to book_path
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
