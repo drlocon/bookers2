ad517a29f027474cf1047c2629901cc9d17cbaba
drlocon <104292397+drlocon@users.noreply.github.com>
2022-05-16 12:42:56 +0000

    [Finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = current_user
+    @user = User.find(params[:id])
     unless @user.id == current_user.id
-      redirect_to books_path
+      redirect_to user_path(current_user)
     end
   end
 
